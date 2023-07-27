ac1c0a3c9c679309d0930f5c07b663e45fae7405
felipekenzo175 <felipekenzoknz@gmail.com>
2023-07-27 11:00:03 -0300

    commit do teste banco de dados

---
+++ TesteBancoDados_01_12072023/TesteBancoDados_01_12072023/TesteBancoDados_01_12072023.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace TesteBancoDados_01_12072023
+{
+    partial class TesteBancoDados_01_12072023
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnMensagem = new System.Windows.Forms.Button();
+            this.btnDesvCond = new System.Windows.Forms.Button();
+            this.btnDesvCondEncadeado = new System.Windows.Forms.Button();
+            this.btnSwitchCase = new System.Windows.Forms.Button();
+            this.btnWhile = new System.Windows.Forms.Button();
+            this.btnFor = new System.Windows.Forms.Button();
+            this.btnForEach = new System.Windows.Forms.Button();
+            this.btnBancoConectado = new System.Windows.Forms.Button();
+            this.btnBancoDesconectado = new System.Windows.Forms.Button();
+            this.lstbxPreferencias = new System.Windows.Forms.ListBox();
+            this.btnDoWhile = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(593, 534);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnDoWhile);
+            this.tabPage1.Controls.Add(this.lstbxPreferencias);
+            this.tabPage1.Controls.Add(this.btnBancoDesconectado);
+            this.tabPage1.Controls.Add(this.btnBancoConectado);
+            this.tabPage1.Controls.Add(this.btnForEach);
+            this.tabPage1.Controls.Add(this.btnFor);
+            this.tabPage1.Controls.Add(this.btnWhile);
+            this.tabPage1.Controls.Add(this.btnSwitchCase);
+            this.tabPage1.Controls.Add(this.btnDesvCondEncadeado);
+            this.tabPage1.Controls.Add(this.btnDesvCond);
+            this.tabPage1.Controls.Add(this.btnMensagem);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(585, 508);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Preferencias";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnMensagem
+            // 
+            this.btnMensagem.Location = new System.Drawing.Point(8, 6);
+            this.btnMensagem.Name = "btnMensagem";
+            this.btnMensagem.Size = new System.Drawing.Size(173, 50);
+            this.btnMensagem.TabIndex = 0;
+            this.btnMensagem.Text = "Clique para Mensagem";
+            this.btnMensagem.UseVisualStyleBackColor = true;
+            this.btnMensagem.Click += new System.EventHandler(this.btnMensagem_Click);
+            // 
+            // btnDesvCond
+            // 
+            this.btnDesvCond.Location = new System.Drawing.Point(8, 56);
+            this.btnDesvCond.Name = "btnDesvCond";
+            this.btnDesvCond.Size = new System.Drawing.Size(173, 50);
+            this.btnDesvCond.TabIndex = 1;
+            this.btnDesvCond.Text = "Clique para Desvio Condicional";
+            this.btnDesvCond.UseVisualStyleBackColor = true;
+            this.btnDesvCond.Click += new System.EventHandler(this.btnDesvCond_Click);
+            // 
+            // btnDesvCondEncadeado
+            // 
+            this.btnDesvCondEncadeado.Location = new System.Drawing.Point(8, 106);
+            this.btnDesvCondEncadeado.Name = "btnDesvCondEncadeado";
+            this.btnDesvCondEncadeado.Size = new System.Drawing.Size(173, 50);
+            this.btnDesvCondEncadeado.TabIndex = 2;
+            this.btnDesvCondEncadeado.Text = "Clique para Desvio Condicional Encadeado";
+            this.btnDesvCondEncadeado.UseVisualStyleBackColor = true;
+            this.btnDesvCondEncadeado.Click += new System.EventHandler(this.btnDesvCondEncadeado_Click);
+            // 
+            // btnSwitchCase
+            // 
+            this.btnSwitchCase.Location = new System.Drawing.Point(8, 156);
+            this.btnSwitchCase.Name = "btnSwitchCase";
+            this.btnSwitchCase.Size = new System.Drawing.Size(173, 50);
+            this.btnSwitchCase.TabIndex = 3;
+            this.btnSwitchCase.Text = "Clique para Switch Case";
+            this.btnSwitchCase.UseVisualStyleBackColor = true;
+            this.btnSwitchCase.Click += new System.EventHandler(this.btnSwitchCase_Click);
+            // 
+            // btnWhile
+            // 
+            this.btnWhile.Location = new System.Drawing.Point(8, 200);
+            this.btnWhile.Name = "btnWhile";
+            this.btnWhile.Size = new System.Drawing.Size(173, 50);
+            this.btnWhile.TabIndex = 4;
+            this.btnWhile.Text = "Clique para While";
+            this.btnWhile.UseVisualStyleBackColor = true;
+            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
+            // 
+            // btnFor
+            // 
+            this.btnFor.Location = new System.Drawing.Point(8, 256);
+            this.btnFor.Name = "btnFor";
+            this.btnFor.Size = new System.Drawing.Size(173, 50);
+            this.btnFor.TabIndex = 5;
+            this.btnFor.Text = "Clique para For";
+            this.btnFor.UseVisualStyleBackColor = true;
+            this.btnFor.Click += new System.EventHandler(this.btnFor_Click);
+            // 
+            // btnForEach
+            // 
+            this.btnForEach.Location = new System.Drawing.Point(8, 306);
+            this.btnForEach.Name = "btnForEach";
+            this.btnForEach.Size = new System.Drawing.Size(173, 50);
+            this.btnForEach.TabIndex = 6;
+            this.btnForEach.Text = "Clique para ForEach";
+            this.btnForEach.UseVisualStyleBackColor = true;
+            this.btnForEach.Click += new System.EventHandler(this.btnForEach_Click);
+            // 
+            // btnBancoConectado
+            // 
+            this.btnBancoConectado.Location = new System.Drawing.Point(8, 356);
+            this.btnBancoConectado.Name = "btnBancoConectado";
+            this.btnBancoConectado.Size = new System.Drawing.Size(173, 50);
+            this.btnBancoConectado.TabIndex = 7;
+            this.btnBancoConectado.Text = "Banco de Dados Conectado";
+            this.btnBancoConectado.UseVisualStyleBackColor = true;
+            this.btnBancoConectado.Click += new System.EventHandler(this.btnBancoConectado_Click);
+            // 
+            // btnBancoDesconectado
+            // 
+            this.btnBancoDesconectado.Location = new System.Drawing.Point(8, 406);
+            this.btnBancoDesconectado.Name = "btnBancoDesconectado";
+            this.btnBancoDesconectado.Size = new System.Drawing.Size(173, 50);
+            this.btnBancoDesconectado.TabIndex = 8;
+            this.btnBancoDesconectado.Text = "Banco de Dados Desconectado";
+            this.btnBancoDesconectado.UseVisualStyleBackColor = true;
+            this.btnBancoDesconectado.Click += new System.EventHandler(this.btnBancoDesconectado_Click);
+            // 
+            // lstbxPreferencias
+            // 
+            this.lstbxPreferencias.FormattingEnabled = true;
+            this.lstbxPreferencias.Location = new System.Drawing.Point(187, 11);
+            this.lstbxPreferencias.Name = "lstbxPreferencias";
+            this.lstbxPreferencias.Size = new System.Drawing.Size(392, 485);
+            this.lstbxPreferencias.TabIndex = 9;
+            // 
+            // btnDoWhile
+            // 
+            this.btnDoWhile.Location = new System.Drawing.Point(8, 458);
+            this.btnDoWhile.Name = "btnDoWhile";
+            this.btnDoWhile.Size = new System.Drawing.Size(173, 50);
+            this.btnDoWhile.TabIndex = 10;
+            this.btnDoWhile.Text = "Clique para DoWhile";
+            this.btnDoWhile.UseVisualStyleBackColor = true;
+            this.btnDoWhile.Click += new System.EventHandler(this.btnDoWhile_Click);
+            // 
+            // TesteBancoDados_01_12072023
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 532);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "TesteBancoDados_01_12072023";
+            this.Text = "Teste Banco Dados 01 - 12/07/2023";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListBox lstbxPreferencias;
+        private System.Windows.Forms.Button btnBancoDesconectado;
+        private System.Windows.Forms.Button btnBancoConectado;
+        private System.Windows.Forms.Button btnForEach;
+        private System.Windows.Forms.Button btnFor;
+        private System.Windows.Forms.Button btnWhile;
+        private System.Windows.Forms.Button btnSwitchCase;
+        private System.Windows.Forms.Button btnDesvCondEncadeado;
+        private System.Windows.Forms.Button btnDesvCond;
+        private System.Windows.Forms.Button btnMensagem;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnDoWhile;
+    }
+}
+
---
+++ TesteBancoDados_01_12072023/TesteBancoDados_01_12072023/TesteBancoDados_01_12072023.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+
+namespace TesteBancoDados_01_12072023
+{
+    public partial class TesteBancoDados_01_12072023 : Form
+    {
+        StreamReader objLeitorTxt;
+        string strLinhaLida;
+
+        OleDbConnection objConexao;
+        OleDbCommand objComando;
+        OleDbDataAdapter objAdaptador;
+        OleDbDataReader objLeitorBD;
+
+        DataTable objTabela;
+
+        public TesteBancoDados_01_12072023()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMensagem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Olá Mundo!");
+        }
+
+        private void btnDesvCond_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Escolha Sim ou Não", "ESCOLHA", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+            {
+                MessageBox.Show("Você escolheu Sim", "OPÇÃO");
+            }
+            else
+            {
+                MessageBox.Show("Você escolheu Não", "OPÇÃO");
+            }
+        }
+
+        private void btnDesvCondEncadeado_Click(object sender, EventArgs e)
+        {
+            DialogResult mensagem = MessageBox.Show("Escolha Sim, Não ou Cancelar", "ESCOLHA", MessageBoxButtons.YesNoCancel);
+
+            if (mensagem == System.Windows.Forms.DialogResult.Yes)
+            {
+                MessageBox.Show("Você escolheu Sim", "OPÇÃO");
+            }
+            else if (mensagem == System.Windows.Forms.DialogResult.No)
+            {
+                MessageBox.Show("Você escolheu Não", "OPÇÃO");
+            }
+            else
+            {
+                MessageBox.Show("Você escolheu Cancelar", "OPÇÃO");
+            }
+        }
+
+        private void btnSwitchCase_Click(object sender, EventArgs e)
+        {
+            switch (MessageBox.Show("Escolha Sim, Não ou Cancelar", "ESCOLHA", MessageBoxButtons.YesNoCancel))
+            {
+                case DialogResult.No:
+                    MessageBox.Show("Você escolheu Não", "OPÇÃO");
+                    break;
+                case DialogResult.Yes:
+                    MessageBox.Show("Você escolheu Sim", "OPÇÃO");
+                    break;
+                case DialogResult.Cancel:
+                    MessageBox.Show("Você escolheu Cancelar", "OPÇÃO");
+                    break;
+                default:
+                    MessageBox.Show("Você escolheu Errado, Escolha Sim, Não ou Cancelar", "OPÇÃO");
+                    break;
+            }
+        }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+            lstbxPreferencias.Items.Clear();
+            impTxtWhile();
+        }
+
+        public void impTxtWhile()
+        {
+            objLeitorTxt = new StreamReader(@"C:\CURSO PROGRAMAR\C#.NET\preferencias.txt");
+            strLinhaLida = objLeitorTxt.ReadLine();
+
+            while (strLinhaLida != null)
+            {
+                lstbxPreferencias.Items.Add(strLinhaLida);
+                strLinhaLida = objLeitorTxt.ReadLine();
+            }
+
+            objLeitorTxt.Close();
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            lstbxPreferencias.Items.Clear();
+            impTxtFor();
+        }
+
+        public void impTxtFor()
+        {
+            objLeitorTxt = new StreamReader(@"C:\CURSO PROGRAMAR\C#.NET\preferencias.txt");
+
+            for (strLinhaLida = objLeitorTxt.ReadLine(); strLinhaLida != null; strLinhaLida = objLeitorTxt.ReadLine())
+            {
+                lstbxPreferencias.Items.Add(strLinhaLida.ToString());
+            }
+
+            objLeitorTxt.Close();
+        }
+
+        private void btnForEach_Click(object sender, EventArgs e)
+        {
+            lstbxPreferencias.Items.Clear();
+            impTxtForEach();
+        }
+
+        public void impTxtForEach()
+        {
+            objLeitorTxt = new StreamReader(@"C:\CURSO PROGRAMAR\C#.NET\preferencias.txt");
+            string[] lista = objLeitorTxt.ReadToEnd().Trim().Split('\n');
+
+            foreach (string item in lista)
+            {
+                 lstbxPreferencias.Items.Add(item.ToString());
+            }
+
+            objLeitorTxt.Close();
+        }
+
+        private void btnBancoConectado_Click(object sender, EventArgs e)
+        {
+            lstbxPreferencias.Items.Clear();
+            impBancoConectado();
+        }
+
+        public void impBancoConectado()
+        {
+            objConexao = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source='C:\CURSO PROGRAMAR\C#.NET\Preferencias_1_05072023.mdb'");
+            objConexao.Open();
+
+            objComando = new OleDbCommand("SELECT Descricao FROM Preferencias_3", objConexao);
+
+            objLeitorBD = objComando.ExecuteReader();
+
+            while (objLeitorBD.Read())
+            {
+                lstbxPreferencias.Items.Add(objLeitorBD["Descricao"].ToString());
+            }
+
+            objConexao.Close();
+            objLeitorBD.Close();
+        }
+
+        private void btnBancoDesconectado_Click(object sender, EventArgs e)
+        {
+            lstbxPreferencias.Items.Clear();
+            impBancoDesconectado();
+        }
+
+        public void impBancoDesconectado()
+        {
+            objConexao = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source='C:\CURSO PROGRAMAR\C#.NET\Preferencias_1_05072023.mdb'");
+
+            objComando = new OleDbCommand("SELECT Descricao FROM Preferencias_3", objConexao);
+
+            objAdaptador = new OleDbDataAdapter(objComando);
+
+            objTabela = new System.Data.DataTable();
+
+            objAdaptador.Fill(objTabela);
+
+            foreach (DataRow item in objTabela.Rows)
+            {
+                lstbxPreferencias.Items.Add(item["Descricao"].ToString());
+            }
+        }
+
+        private void btnDoWhile_Click(object sender, EventArgs e)
+        {
+            lstbxPreferencias.Items.Clear();
+            impTxtDoWhile();
+        }
+
+        public void impTxtDoWhile()
+        {
+            objLeitorTxt = new StreamReader(@"C:\CURSO PROGRAMAR\C#.NET\preferencias.txt");
+            strLinhaLida = objLeitorTxt.ReadLine();
+
+            do
+            {
+                lstbxPreferencias.Items.Add(strLinhaLida);
+                strLinhaLida = objLeitorTxt.ReadLine();
+            } while (strLinhaLida != null);
+
+            objLeitorTxt.Close();
+        }
+    }
+}
